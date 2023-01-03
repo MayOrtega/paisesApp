3fb73c4dad7d1e4989c81d013802760327c4b9f9
MayorlenO <mayorlen.ortega@gmail.com>
2023-01-03 11:48:53 -0300

    print country list

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,17 +10,21 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 termino: string = '';
 hayError: boolean = false;
+paises: Country[] = [];
 
 constructor( private paisService: PaisService){}
 
 buscar(){
  
   console.log(this.termino);
-  this.paisService.buscarPais(this.termino).subscribe(resp => {
+  this.paisService.buscarPais(this.termino).subscribe((paises) => {
     this.hayError = false;
-    console.log(resp)
+    this.paises = paises;
+    console.log(paises)
+
   }, (err) => {
     this.hayError = true;
+    this.paises = [];
     console.log('Error')
     console.log(err)
   })
